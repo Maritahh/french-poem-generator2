b588b2c5bcdb85aff880e4f5b6e3b14edb1d3b52
Marita <mhegertun@gmail.com>
2026-07-30 13:25:14 +0200

    Improved UX, updated html, css and js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("poem generated");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,18 +12,14 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "f10038571b3e14038ft5b2o6ea2a4fab";
-  let prompt = `User instructions: Generate a Norwegian poem about ${instructionsInput.value}: `;
+  let prompt = `User instructions: Generate a Norwegian poem about ${instructionsInput.value} `;
   let context =
-    "You are a romantic poem expert and write beautiful Norwegian poems. Your mission is to create a poem in basic HTML and separate lines with <br>. " +
-    "Make sure to follow the user instructions. Do not include any other text or explanations, only the poem itself. " +
-    "The poem should be written in a romantic style without lines, symbols or special characters. " +
-    "The poem should be at least 4 lines long. " +
-    "The poem should be written in Norwegian language.";
+    "You are a romantic poem expert and write beautiful Norwegian poems. Your mission is to create a poem in simple and separate lines with <br>. Start each sentence with capital letter. The poem should be written in a romantic style. Sign the poem with 'Generator kodet av Marita Hamran via SheCodes AI Api' inside a <strong> element at the end of the poem ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Lager et norsk dikt om ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
